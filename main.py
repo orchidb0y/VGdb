f272de5f5845dab57472567fd9308b06c34fd6e7
David Sobral <davidsobral@me.com>
2022-07-03 12:31:22 -0300

    Refactor search by rating for more complete return

--- main.py
+++ main.py
@@ -104,38 +104,39 @@ Pick a rating from 1-100: ''')
             rating = input('Please enter a number between 1-100: ')
 
         list_of_games = rating_map.retrieve(rating)
-        current_list = len(list_of_games)
-        remaining = 10 - current_list
-        if list_of_games != []:
-            for i in range(current_list):
-                print(f'''
-        Name: {list_of_games[0][0]}
-Release date: {list_of_games[0][1]}
-      Rating: {list_of_games[0][2]}
-      Genres: {list_of_games[0][3]}
-   Platforms: {list_of_games[0][4]}
+        remaining = 10
+        shuffle(list_of_games)
+        while True:
+            list_of_games = list_of_games[:10]
+            remaining = remaining - len(list_of_games)
+            if list_of_games != []:
+                for game in list_of_games:
+                    print(f'''
+        Name: {game[0]}
+Release date: {game[1]}
+      Rating: {game[2]}
+      Genres: {game[3]}
+   Platforms: {game[4]}
 ''')
+            if len(list_of_games) < 10:
+                break
 
         while remaining > 0:
             rating = int(rating) + 1
             if rating == 101:
                 break
             list_of_games = rating_map.retrieve(rating)
-            while len(list_of_games) > remaining:
-                list_of_games.pop()
-            current_list = len(list_of_games)
-            remaining = remaining - current_list
-            
+            list_of_games = list_of_games[:10]
+            remaining = remaining - len(list_of_games)
             if list_of_games != []:
-                for i in range(current_list):
+                for game in list_of_games:
                     print(f'''
-        Name: {list_of_games[0][0]}
-Release date: {list_of_games[0][1]}
-      Rating: {list_of_games[0][2]}
-      Genres: {list_of_games[0][3]}
-   Platforms: {list_of_games[0][4]}
+        Name: {game[0]}
+Release date: {game[1]}
+      Rating: {game[2]}
+      Genres: {game[3]}
+   Platforms: {game[4]}
 ''')
-                    list_of_games.pop(0)
     
         self.cont()
             
