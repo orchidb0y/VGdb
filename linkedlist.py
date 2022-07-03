a2ee974584bc564fc638fc08ce5930cc7a6bebbc
David Sobral <davidsobral@me.com>
2022-07-03 09:38:46 -0300

    Readd method accidentally removed in previous commit

--- linkedlist.py
+++ linkedlist.py
@@ -10,6 +10,9 @@ class Node:
   
     def get_next_node(self):
         return self.next_node
+    
+    def set_next_node(self, next_node):
+        self.next_node = next_node
 
 
 class LinkedList:
